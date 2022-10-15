265f360546aeca91e37cbd63395ffafe9ddaa796
iStre1ok <am-nf@yandex.ru>
2022-10-15 17:20:28 +0300

    rewrite carousel on native js

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick();
-  });
+const slider = tns({
+  container: '.carousel__inner',
+  nav: false,
+  controls: false,
+  speed: 1500
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+});
