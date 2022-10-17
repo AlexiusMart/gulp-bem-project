373e6d4663b6a8e24dfe232f8bea94a9e88ed19c
iStre1ok <am-nf@yandex.ru>
2022-10-17 22:42:33 +0300

    EROOOORRRR((((

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const slider = tns({
   container: '.carousel__inner',
   nav: false,
@@ -11,3 +13,21 @@ document.querySelector('.prev').addEventListener('click', function () {
 document.querySelector('.next').addEventListener('click', function () {
   slider.goTo('next');
 });
+
+
+const tabs = document.getElementsByClassName('catalog__tab');
+const sections = document.getElementsByClassName('catalog__content');
+
+[...tabs].forEach(tab => tab.addEventListener('click', tabClick));
+
+function tabClick(event) {
+  const tabId = event.target.dataset.id;
+  
+  [...tabs].forEach((tab, i) => {
+    tab.classList.remove('catalog__tab_active');
+    sections[i].classList.remove('catalog__content_active');
+  })
+  
+  tabs[tabId - 1].classList.add('catalog__tab_active');
+  sections[tabId - 1].classList.add('catalog__content_active');
+}
